aebc856ca4dd20ae7a7fa37ee6b4335cade708c0
nahasmampad <nahasbinasker@gmail.com>
2023-01-09 10:22:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000 ",
+  },
+});
+
+let users = [];
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+
+io.on("connection", (socket) => {
+  //when connected
+  console.log("user connected");
+  //take user id socket id from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get messages
+  socket.on("sendMessage", ({ senderId, reciverId, text }) => {
+    const user = getUser(reciverId)
+    console.log('user->', user);
+    io.to(user.socketId).emit('getMessage', {
+        senderId,
+        text
+    })
+  });
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
